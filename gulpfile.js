faa10ad09780d9b071a193432baebc9b926f5acb
jackliu <1336101787@qq.com>
2019-07-27 20:18:20 +0800

    构建框架

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const htmlmin = require("gulp-htmlmin");
+gulp.task("htmlmin", function () {
+    gulp.src(["./src/*.html", "./src/*.htm"])
+        .pipe(htmlmin({
+            removeComments: true, //清除HTML注释
+            collapseWhitespace: true, //压缩HTML
+            collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input />
+            removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+            removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
+            removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+            minifyJS: true, //压缩页面JS
+            minifyCSS: true //压缩页面CSS
+        }))
+        .pipe(gulp.dest("./dist"));
+});
+gulp.task("default",["htmlmin"]);
