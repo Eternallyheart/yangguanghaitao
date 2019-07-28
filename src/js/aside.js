6bf969a89d0a1eaf2eff2694b20e5343b75d7792
jackliu <1336101787@qq.com>
2019-07-28 21:51:35 +0800

    新增头部,尾部,边缘页面

---
+++ src/js/aside.js
@@ -0,0 +1,20 @@
+$(".toolbar_person").on("mouseenter",function(){
+    $(this).addClass("current").siblings(".toolbar_person").removeClass("current");
+    console.log($(".user"))
+    console.log($(this).index())
+    if($(this).index()==2){
+        $(".user").eq(1).stop().animate({right:35}).siblings(".user").animate({right:-35});
+        $(".user").eq(1).addClass("current").siblings(".user").removeClass("current");
+    }else  if($(this).index()==0){
+         $(".user").eq(0).stop().animate({right:35}).siblings(".user").animate({right:-35});
+         $(".user").eq(0).addClass("current").siblings(".user").removeClass("current");
+    }
+}).on("mouseleave",function(){
+    $(".toolbar_person").removeClass("current");
+    $(".user").stop().animate({right:-35});
+})
+$(".toolbar_scroll").on("mouseenter",function(){
+    $(".scroll").addClass("current").stop().animate({right:35});
+}).on("mouseleave",function(){
+    $(".scroll").removeClass("current").stop().animate({right:-45});
+})
