2c330c7d1bb1be6d9b9662ca9252b2d73f4a6ae3
jackliu <1336101787@qq.com>
2019-08-01 11:54:26 +0800

    首页js行为构建

--- src/js/index.js
+++ src/js/index.js
@@ -24,10 +24,15 @@ $(() => {
     }
     let timer = setInterval(autoPlay, 3000);
     $(".banner_show>span").on("click", function () {
-        console.log($(this).index())
-        clearInterval(timer)
+        console.log($(this).index());
+        clearInterval(timer);
         index = $(this).index()-1;
         autoPlay();
+        timer = setInterval(autoPlay, 3000);
+    })
+    $(".in_banner>main").on("mouseenter",function(){
+        clearInterval(timer);
+    }).on("mouseleave",function(){
         timer = setInterval(autoPlay, 3000);
     })
 })
@@ -43,5 +48,73 @@ $(()=>{
 $(()=>{
     $(".fi_pi_main>figure").on("mouseenter",function(){
         $(this).addClass("current").siblings().removeClass("current");
+    }).on("mouseleave",function(){
+        $(".fi_pi_main>figure").removeClass("current");
+    })
+})
+//skin_care 侧边栏点击移动切换链接效果
+$(()=>{
+    let index=0;
+    $(".skin_care>.fi_pi_right span").eq(0).on("click",function(){
+        index++;
+        if(index>=$(".skin_care .fi_pi_move>li").length-4){
+            index=$(".skin_care .fi_pi_move>li").length-4;
+        }
+        $(this).nextAll(".fi_pi_box").children(".fi_pi_move").animate({top:-60*index});
+    })
+    $(".skin_care>.fi_pi_right span").eq(1).on("click",function(){
+        index--;
+        if(index<=0){
+            index=0;
+        }
+        $(this).next().children(".fi_pi_move").animate({top:-60*index});
+    })
+})
+$(()=>{
+    let index=0;
+    $(".skin_care>.fi_pi_right span").eq(2).on("click",function(){
+        index++;
+        if(index>=$(".skin_care .fi_pi_move>li").length-4){
+            index=$(".skin_care .fi_pi_move>li").length-4;
+        }
+        $(this).nextAll(".fi_pi_box").children(".fi_pi_move").animate({top:-60*index});
+    })
+    $(".skin_care>.fi_pi_right span").eq(3).on("click",function(){
+        index--;
+        if(index<=0){
+            index=0;
+        }
+        $(this).next().children(".fi_pi_move").animate({top:-60*index});
+    })
+})
+//mall_infor晒单轮播图
+$(()=>{
+    let index=0;
+    function newAutoPlay(){
+        index++;
+        if(index>=$(".mall_list_all>li").length-2){
+            index=0;
+        }
+        $(".mall_list_all").animate({left:index*-200});
+    }
+    let newTimer = setInterval(newAutoPlay, 3000);
+    $(".list_left").on("click", function () {
+        clearInterval(newTimer);
+        index-=2;
+        if(index==-2){
+            index=-1;
+        }
+        newAutoPlay();
+        newTimer = setInterval(newAutoPlay, 3000);
+    })
+    $(".list_right").on("click", function () {
+        clearInterval(newTimer);
+        newAutoPlay();
+        newTimer = setInterval(newAutoPlay, 3000);
+    })
+    $(".mall_list_all").on("mouseenter",function(){
+        clearInterval(newTimer);
+    }).on("mouseleave",function(){
+        newTimer = setInterval(newAutoPlay, 3000);
     })
 })
