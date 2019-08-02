17363b8f70e8af8e3151b04f14cbc83aaf7d1c8e
jackliu <1336101787@qq.com>
2019-08-02 11:54:58 +0800

    详情页js行为搭建 8/2

--- src/js/datails.js
+++ src/js/datails.js
@@ -56,6 +56,11 @@ $(() => {
     })
 })
 
+//点击收藏
+$(()=>{
+    $(".save_product").on("click",function(){})
+})
+
 //选择地址 省份显示
 $(() => {
     $(".area button").on("mouseenter", function () {
@@ -65,7 +70,7 @@ $(() => {
     $(".area").on("mouseleave", function () {
         $(this).children("button").removeClass("current");
     })
-    $(".area_address").on("mouseenter",function(){
+    $(".area_address").on("mouseenter", function () {
         $(".area").children("button").addClass("current");
     })
     $(".area_address").on("mouseleave", function () {
@@ -74,6 +79,117 @@ $(() => {
     })
 })
 //点击切换地址
-$(()=>{
-    $(".select_province a").on("click")
+$(() => {
+    $(".select_province a").on("click", function () {
+        let strProvince = $(this).text();
+        $(".province").html(strProvince);
+        $(".select_address").text(strProvince);
+        $(this).parent().hide();
+        $(this).parent().next("ul").show().siblings("ul").hide();
+        $(".select_city a").on("click", function () {
+            let strCity = $(this).text();
+            $(".city").html(strCity);
+            $(".select_address").text(strProvince + " " + strCity);
+            $(this).parent().hide();
+            $(this).parent().next("ul").show().siblings("ul").hide();
+
+            $(".select_county a").on("click", function () {
+                let strCounty = $(this).text();
+                $(".county").html(strCounty);
+                $(".select_address").text(strProvince + " " + strCity + " " + strCounty);
+                $(this).parent().hide();
+                $(this).parent().next("ul").show().siblings("ul").hide();
+
+                $(".select_road a").on("click", function () {
+                    let strRoad = $(this).text();
+                    $(".road").html(strRoad);
+                    $(".select_address").text(strProvince + " " + strCity + " " + strCounty + " " + strRoad);
+                    $(this).parent().hide();
+                    
+                    $(".area button").on("mouseenter", function () {
+                        $(".select_province").show();
+                        $(".area_address").find("span").eq(0).addClass("current");
+                    })
+                    
+                    $(".area_address").find("span").on("click", function () {
+                        $(this).addClass("current").siblings().removeClass("current");
+                        $(".area_address>ul").eq($(this).index()).show().siblings("ul").hide();
+                    })
+                })
+            })
+        })
+    })
 })
+//点击增加购买数量
+$(()=>{
+    let buyNum=$(".det_ch_cart").text();
+    $(".det_ch_add").on("click",function(){
+        buyNum++;
+        if(buyNum>=$(".det_ch_stock").text()){
+            buyNum=$(".det_ch_stock").text();
+        }
+        $(".det_ch_cart").text(buyNum);
+    })
+    $(".det_ch_reduce").on("click",function(){
+        buyNum--;
+        if(buyNum<=1){
+            buyNum=1;
+        }
+        $(".det_ch_cart").text(buyNum);
+    })
+})
+//点击购买
+
+
+//点击加入购物车
+
+
+//热销推荐 鼠标悬停 显示分享
+$(()=>{
+    $(".details_banner").find("figure").on("mouseenter",function(){
+        $(this).append($(".banner_share"));
+        $(".banner_share").show();
+    }).on("mouseleave",function(){
+        $(this).remove($(".banner_share"));
+        $(".banner_share").hide();
+    })
+})
+
+//内容图片 鼠标悬停 显示分享
+$(()=>{
+    $(".su_con_pro_img").on("mouseenter",function(){
+        $(this).append($(".content_share"));
+        $(".content_share").show();
+    }).on("mouseleave",function(){
+        $(this).remove($(".content_share"));
+        $(".content_share").hide();
+    })
+})
+
+//相关商品 鼠标悬停 显示分享
+$(()=>{
+    $(".det_re_product").find("figure").on("mouseenter",function(){
+        console.log($(".relate_share"))
+        $(this).append($(".relate_share"));
+        $(".relate_share").show();
+    }).on("mouseleave",function(){
+        $(this).remove($(".relate_share"));
+        $(".relate_share").hide();
+    })
+})
+
+//产品介绍 tab栏切换
+$(()=>{
+    $(".pro_su_title").find("li").on("click",function(){
+        console.log($(".pro_su_content"))
+        $(this).addClass("current").siblings().removeClass("current");
+        $(".pro_su_content>div").eq($(this).index()).addClass("current").siblings().removeClass("current");
+    })
+    $(".su_con_app_title").find("li").on("click",function(){
+        console.log($(".su_con_app_content"))
+        $(this).addClass("show").siblings().removeClass("show");
+        $(".su_con_app_content>ul").eq($(this).index()).addClass("show").siblings().removeClass("show");
+    })
+})
+
+//滑动条滑动 导航栏固定
